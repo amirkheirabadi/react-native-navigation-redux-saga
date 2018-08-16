dbef53e1140538825151bef3a0480b3b561034f4
Fattah Muhyiddeen <fattahmuhyiddeen@gmail.com>
2018-08-16 15:27:58 +0800

    add react navigation v2

---
+++ src/navigation.js
@@ -0,0 +1,13 @@
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import HomeScreen from './modules/home/home'
+export const StackNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+});
+
+export const DrawerNavigator = createDrawerNavigator({
+    Home: {
+      screen: HomeScreen
+    },
+  });
